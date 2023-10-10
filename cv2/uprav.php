961e0bd3146a3d6a63e241fc8d222d7fb9df8b2d
Ales Jandera <ales.jandera@gmail.com>
2023-10-10 12:20:15 +0200

    Uopdate uprav.

--- cv2/uprav.php
+++ cv2/uprav.php
@@ -1,34 +1,37 @@
 <?php
 session_start();
 
-include"common.php";
-include "spoj.php";
+require "common.php";
 
 $rowid = $_REQUEST["rowid"];
-$selectStmt ="select * from $tableName where rowid=$rowid";
 
-if(!($result=mysql_query($selectStmt)))
- {
-  DisplayErrMsg("chyba vo vykonávaní $selectStmt stmt");
+try {
+  $stmt = $pdo->prepare("SELECT * from address where ROWID = ?");
+  $stmt->bindParam(1, $rowid);
+  $stmt->execute();
+  $row = $stmt->fetch();
+} catch (PDOException $e) {
+  displayErrMsg("Error: " . $e->getMessage());
   exit();
 }
 
-if ($_SESSION['lan']=="sk") GenerateHTMLHeader("Zmena údajov","Prosím modifikujte polia");
-else GenerateHTMLHeader("Entry modification","Please modify fields");
-if(!($row=mysql_fetch_object($result))){
-  if ($_SESSION['lan']=="sk") DisplayErrMsg("Interná chyba : záznam neexistuje");
-  else DisplayErrMsg("Internal error : entry not found");
-  exit();
+if ($_SESSION['lan'] == "sk") {
+  generateHTMLHeader("Zmena udajov","Prosim modifikujte polia");
+} else {
+  generateHTMLHeader("Entry modification","Please modify fields");
 }
-$resultEntry["id"]=$row->ROWID;
-$resultEntry["cn"]=$row->MENO;
-$resultEntry["mail"]=$row->EMAIL;
-$resultEntry["locality"]=$row->MESTO;
-$resultEntry["description"]=$row->POPIS;
-$resultEntry["number"]=$row->TELEFON;
+
+$resultEntry["id"]=$row['ROWID'];
+$resultEntry["cn"]=$row['MENO'];
+$resultEntry["mail"]=$row['EMAIL'];
+$resultEntry["locality"]=$row['MESTO'];
+$resultEntry["description"]=$row['POPIS'];
+$resultEntry["number"]=$row['TELEFON'];
 
 
-if ($_SESSION['lan']=="sk") GenerateHtmlForm($resultEntry,"update.php?rowid=$rowid","ZMEN");
-else GenerateHtmlForm($resultEntry,"update.php?rowid=$rowid","MODIFY");
-mysql_free_result($result);
+if ($_SESSION['lan']=="sk") {
+  generateHtmlForm($resultEntry,"update.php?rowid=$rowid","ZMEN");
+} else {
+  generateHtmlForm($resultEntry,"update.php?rowid=$rowid","MODIFY");
+}
 ?>
